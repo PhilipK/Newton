e08a18bd188152d76d9bc27e8d08306a9f00e51c
Philip <philipkristoffersen@gmail.com>
2020-06-21 21:55:01 +0200

    Removing automatic zooming again.

--- src/systems/camera.rs
+++ src/systems/camera.rs
@@ -1,13 +1,10 @@
 use crate::components::Player;
-use crate::components::Velocity;
-use crate::playercamera::{CAMERA_HEIGHT, CAMERA_WIDTH};
 
 use amethyst::{
-    core::timing::Time,
     core::Transform,
     derive::SystemDesc,
-    ecs::{Entities, Join, Read, ReadStorage, System, SystemData, WriteStorage},
-    renderer::{camera::Projection, Camera},
+    ecs::{Entities, Join, ReadStorage, System, SystemData, WriteStorage},
+    renderer::Camera,
 };
 
 #[derive(SystemDesc)]
@@ -15,27 +12,19 @@ pub struct CameraSystem;
 
 impl<'s> System<'s> for CameraSystem {
     type SystemData = (
-        WriteStorage<'s, Camera>,
+        ReadStorage<'s, Camera>,
         WriteStorage<'s, Transform>,
         ReadStorage<'s, Player>,
-        ReadStorage<'s, Velocity>,
         Entities<'s>,
     );
 
-    fn run(
-        &mut self,
-        (mut cameras, mut transforms, players, velocities, entities): Self::SystemData,
-    ) {
+    fn run(&mut self, (cameras, mut transforms, players, entities): Self::SystemData) {
         // let delta_time = time.delta_seconds();
-        for (player_entity, _player, player_velocity) in (&entities, &players, &velocities).join() {
+        for (player_entity, _player) in (&entities, &players).join() {
             if let Some(player_transform) = transforms.get(player_entity) {
                 let player_position = player_transform.translation().clone();
-                for (camera_entity, camera) in (&entities, &mut cameras).join() {
+                for (camera_entity, _camera) in (&entities, &cameras).join() {
                     if let Some(camera_transform) = transforms.get_mut(camera_entity) {
-                        let velocity = player_velocity.velocity;
-                        let scale = f32::min(f32::max(1.0, velocity.magnitude() / 100.0), 3.0);
-                        let scaled_projection = get_scaled_camera_projection(scale);
-                        camera.set_projection(scaled_projection);
                         camera_transform.set_translation_xyz(
                             player_position.x,
                             player_position.y,
@@ -46,15 +35,4 @@ impl<'s> System<'s> for CameraSystem {
             }
         }
     }
-}
-
-fn get_scaled_camera_projection(scale: f32) -> Projection {
-    Projection::orthographic(
-        -CAMERA_WIDTH / 2.0 * scale,
-        CAMERA_WIDTH / 2.0 * scale,
-        -CAMERA_HEIGHT / 2.0 * scale,
-        CAMERA_HEIGHT / 2.0 * scale,
-        0.1,
-        2000.0,
-    )
 }
