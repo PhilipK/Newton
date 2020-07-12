0ad90c9617942c6570b9d298d877579a56e9a36e
Philip <philipkristoffersen@gmail.com>
2020-07-12 22:21:15 +0200

    Explanation on title screen

--- src/entities/title_text.rs
+++ src/entities/title_text.rs
@@ -21,16 +21,6 @@ pub fn itnitialize_title_text(world: &mut World) {
         500.,
         100.,
     );
-    let transform_explain = UiTransform::new(
-        "NEWTON_EXPLAIN".to_string(),
-        Anchor::Middle,
-        Anchor::Middle,
-        0.,
-        -100.,
-        1.,
-        600.,
-        50.,
-    );
     let title = world
         .create_entity()
         .with(transform)
@@ -41,16 +31,51 @@ pub fn itnitialize_title_text(world: &mut World) {
             100.,
         ))
         .build();
+
+    let transform_explain = UiTransform::new(
+        "NEWTON_EXPLAIN".to_string(),
+        Anchor::Middle,
+        Anchor::Middle,
+        0.,
+        -100.,
+        1.,
+        600.,
+        50.,
+    );
     let title_explain = world
         .create_entity()
         .with(transform_explain)
         .with(UiText::new(
-            font,
+            font.clone(),
             "Push W to start".to_string(),
             [1., 1., 1., 1.],
             50.,
         ))
         .build();
+
+    let transform_buttons = UiTransform::new(
+        "NEWTON_BUTTONS".to_string(),
+        Anchor::Middle,
+        Anchor::Middle,
+        0.,
+        -300.,
+        1.,
+        800.,
+        50.,
+    );
+    let title_buttons = world
+        .create_entity()
+        .with(transform_buttons)
+        .with(UiText::new(
+            font,
+            "W=Thrust  A=Rotate Left  D=Rotate Right  Scroll=Zoom".to_string(),
+            [1., 1., 1., 1.],
+            25.,
+        ))
+        .build();
+
+    
     world.insert(title);
     world.insert(title_explain);
+    world.insert(title_buttons);
 }
